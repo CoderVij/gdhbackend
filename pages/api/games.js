b609feb0702a80ec7198ac87358bb8d8a6619e50
CoderVij <contactfreakout@gmail.com>
2025-09-24 18:42:03 +0530

    developer_name added to games

--- pages/api/games.js
+++ pages/api/games.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
     try {
       const {
         developer_email,
+        developer_name,
         game_name,
         store_url_android,
         store_url_ios,
